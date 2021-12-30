f3b1a64a55f754f4b28308ec392869b858c955eb
Glitch (glitch-hello-node) <none>
2021-12-30 08:07:27 +0000

    🏠🎙 Checkpoint ./src/index.html:35820864/635 ./src/script.js:35820864/170 ./db/data.json:35820864/256

--- src/script.js
+++ src/script.js
@@ -1,11 +1,2 @@
 console.log("Script is running");
 
-$(document).ready(() => {
-    $('teacher-select').selectize({
-        sortField: 'text'
-    });
-  
-    $('class-select').selectize({
-        sortField: 'text'
-    });
-});
