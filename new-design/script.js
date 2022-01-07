c60b515799102944a3ace31b747db2c6dabc41e0
Armatech_Team <armatech20@gmail.com>
2022-01-07 15:43:20 +0300

    123
    123

---
+++ new-design/script.js
@@ -0,0 +1,20 @@
+let marquee = document.getElementById("header");
+
+function resize(){
+    let innerWidth = window.innerWidth;
+
+    if(1200 < innerWidth){
+        marquee.setAttribute("scrollamount", "10");
+    }
+
+    else if(900 <= innerWidth <= 1200){
+        marquee.setAttribute("scrollamount", "5");
+    }
+
+    else if(900 > innerWidth){
+        marquee.setAttribute("scrollamount", "1");
+    }
+}
+
+resize();
+window.onresize = resize;
